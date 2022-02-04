08b24cb562cd52c180502356462b2540d9f7bf52
hamidfk <hamidfathoni89@gmail.com>
2022-02-04 22:44:08 +0700

    Menambahkan paragraf

--- coba_git_remote.php
+++ coba_git_remote.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque culpa reprehenderit deleniti ullam sit doloribus.</p>
 </body>
 </html>
